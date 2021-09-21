3b7ac62f079a51ef0cf76b9a1a671e8debcda1a4
Spider X <abirali20205@gmail.com>
2021-09-21 21:11:19 +0600

    Sherlock-lite

---
+++ main.py
@@ -0,0 +1,110 @@
+#sherlock_lite
+
+#Font
+bold_start = '\033[1m'
+bold_end = '\033[0m'
+
+print()
+print(bold_start, "The Uses is Simple, Just Type The Name Of That Target :D", bold_end)
+print()
+user_name = input("\033[1;36;40m Enter the Target Name: ")
+
+#Social Media
+print()
+print(bold_start, "-------Social Media Platforms-------", bold_end)
+print()
+
+print(bold_start, "Facebook: ", bold_end, " https://www.facebook.com/"+user_name)
+print(bold_start, "Instagram: ", bold_end, "https://www.instagram.com/"+user_name)
+print(bold_start, "Twitter: ", bold_end, "  https://www.twitter.com/"+user_name)
+print(bold_start, "Tik-Tok: ", bold_end, "  https://www.tiktok.com/"+user_name)
+print(bold_start, "Snap-Chat: ", bold_end, "https://www.snapchat.com/"+user_name)
+print(bold_start, "Tumblr: ", bold_end, "   https://www.tumblr.com/"+user_name)
+print(bold_start, "Reddit: ", bold_end, "   https://www.reddit.com/"+user_name)
+print(bold_start, "Xing: ", bold_end, "     https://www.xing.com/"+user_name)
+
+#Dating Platforms
+print()
+print(bold_start, "-------Dating Platforms-------", bold_end)
+print()
+
+print(bold_start, "Tinder: ", bold_end, "     https://tinder.com/"+user_name)
+print(bold_start, "Match: ", bold_end, "      https://www.match.com/"+user_name)
+
+#Music Sharing Platforms
+print()
+print(bold_start, "-------Music Sharing Platforms-------", bold_end)
+print()
+
+print(bold_start, "SoundCloud: ", bold_end, " https://soundcloud.com/"+user_name)
+print(bold_start, "Audio-mack: ", bold_end, " https://audiomack.com/"+user_name)
+print(bold_start, "iTunes: ", bold_end, "     https://www.apple.com/itunes/"+user_name)
+print(bold_start, "Spotify: ", bold_end, "    https://www.spotify.com/"+user_name)
+print(bold_start, "BandCamp: ", bold_end, "   https://bandcamp.com/"+user_name)
+print(bold_start, "Tidal: ", bold_end, "      https://tidal.com/"+user_name)
+
+#Video Sharing Platform
+print()
+print(bold_start, "-------Video Sharing Platforms-------", bold_end)
+print()
+
+print(bold_start, "Twitch: ", bold_end, "     https://www.twitch.tv/"+user_name)
+print(bold_start, "Youtube: ", bold_end, "    https://www.youtube.com/"+user_name)
+
+#Code Sharing Platforms
+print()
+print(bold_start, "-------Code Sharing Platforms-------", bold_end)
+print()
+
+print(bold_start, "Github: ", bold_end, "     https://www.github.com/"+user_name)
+
+#Image Sharing platforms
+print()
+print(bold_start, "-------Image Sharing Platforms-------", bold_end)
+print()
+
+print(bold_start, "Pinterest: ", bold_end, "  https://www.pinterest.com/"+user_name)
+print(bold_start, "Flickr: ", bold_end, "     https://www.flickr.com/"+user_name)
+
+#Official Platforms
+print()
+print(bold_start, "-------Official Platforms-------", bold_end)
+print()
+
+print(bold_start, "linkedIn: ", bold_end, "   https://www.linkedin.com/"+user_name)
+print(bold_start, "JobCase: ", bold_end, "    https://www.jobcase.com/"+user_name)
+print(bold_start, "AngelList: ", bold_end, "  https://angel.co/"+user_name)
+
+#QnA based Websites
+print()
+print(bold_start, "-------QnA Based Platforms-------", bold_end)
+print()
+
+print(bold_start, "Quora: ", bold_end, "      https://www.quora.com/"+user_name)
+print(bold_start, "Ask: ", bold_end, "        https://ask.fm/"+user_name)
+
+#Ending
+print("\n")
+print("-----Attack Done!-----")
+
+print("          *  ")
+print("        ** **")
+print("      ***   *** ")
+print("        ** ** ")
+print("          * ")
+print("          *")
+print("          *")
+print("         ***")
+print("        * * *")
+print("      *   *   *")
+print("     *    *     *")
+print(" *  *     *      *  *")
+print("  *       *        *")
+print("          *        ")
+print("          *        ")
+print("          *        ")
+print("        *   *       ")
+print("      *       *      ")
+print("    *           *    ")
+print("  *               *   ")
+print("*                   *    ")
